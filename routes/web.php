85604fc5968e6f57525878649142d9be3166adb5
운이 좋은 <102027374+LuckyMendoza@users.noreply.github.com>
2025-06-13 22:33:48 -1200

    Add GitHub Action to run artisan after deploy

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home.index');
+
+Route::get('/run-artisan', function () {
+    if (
+        app()->environment('production') &&
+        request()->query('key') === env('DEPLOY_KEY')
+    ) {
+        Artisan::call('optimize:clear');
+        Artisan::call('config:cache');
+        Artisan::call('migrate', ['--force' => true]);
+        return '✔ Artisan commands executed!';
+    }
+
+    abort(403);
+});
